157bf925b9b4b57874fca0db5ea25623863f3cdb
ovinan <67182108+ovinan@users.noreply.github.com>
2023-01-05 16:22:31 +0100

    Oracle orient. a objetos (modulo: Acceso a Datos)
    Ejemplos SQL de Oracle Orientado a Objetos, para su utilización en la asignatura de Acceso a Datos

---
+++ ejemplo oracle orientado a objetos (no funcional).sql
@@ -0,0 +1,62 @@
+CREATE TYPE lista_tel_t AS VARRAY(10) OF VARCHAR2(20);
+/
+CREATE TYPE direccion_t AS OBJECT (
+	calle VARCHAR2(200),
+	ciudad VARCHAR2(200),
+	prov CHAR(2),
+	codpos VARCHAR2(20)
+) ;
+/
+CREATE TYPE clientes_t AS OBJECT (
+	clinum NUMBER,
+	clinomb VARCHAR2(200),
+	direccion direccion_t,
+	lista_tel lista_tel_t
+) ;
+/
+CREATE TYPE item_t AS OBJECT (
+	itemnum NUMBER,
+	precio NUMBER,
+	tasas NUMBER
+) ;
+/
+CREATE TYPE linea_t AS OBJECT (
+	linum NUMBER,
+	item REF item_t,
+	cantidad NUMBER,
+	descuento NUMBER
+) ;
+/
+CREATE TYPE lineas_pedido_t AS TABLE OF linea_t ;
+/
+CREATE TYPE ordenes_t AS OBJECT (
+	ordnum NUMBER,
+	cliente REF clientes_t,
+	fechpedido DATE,
+	fechentrega DATE,
+	pedido lineas_pedido_t,
+	direcentrega direccion_t
+) ;
+/
+CREATE TABLE clientes_tab OF clientes_t
+ (clinum PRIMARY KEY);
+/
+CREATE TABLE items_tab OF item_t
+ (itemnum PRIMARY KEY) ;
+/
+CREATE TABLE ordenes_tab OF ordenes_t (
+PRIMARY KEY (ordnum),
+SCOPE FOR (cliente) IS clientes_tab
+) NESTED TABLE pedido STORE AS pedidos_tab ;
+/
+ALTER TABLE pedidos_tab ADD (SCOPE FOR (item) IS items_tab) ;
+/
+-- ESTA INSTRUCCION FALLA:
+INSERT INTO clientes_tab
+ VALUES (
+ 1, "Lola Caro",
+ direccion_t("12 Calle Lisboa", "Nules", "CS", "12678"),
+ lista_tel_t("415-555-1212")
+ ) ;
+
+
---
+++ ejemplo oracle orientado a objetos.sql
@@ -0,0 +1,38 @@
+/* EJEMPLO SENCILLO CREACION DE OBJETOS CON METODOS Y EJECUCION DE LOS METODOS */
+/* Creamos el objeto TIPO_CUBO, con 3 atributos y 2 metodos */
+CREATE OR REPLACE TYPE Tipo_Cubo AS OBJECT (
+    largo INTEGER,
+    ancho INTEGER,
+    alto INTEGER,
+    MEMBER FUNCTION superficie RETURN INTEGER,
+    MEMBER FUNCTION volumen RETURN INTEGER
+);
+/
+/* Programamos el codigo de los metodos */
+CREATE OR REPLACE TYPE BODY Tipo_Cubo AS
+    MEMBER FUNCTION volumen RETURN INTEGER IS
+    BEGIN
+        RETURN largo * ancho * alto;
+    END;
+    MEMBER FUNCTION superficie RETURN INTEGER IS
+    BEGIN
+        RETURN 2 * (largo * ancho + largo * alto + ancho * alto);
+    END;
+END;
+/
+/* Creamos la tabla de objetos */
+CREATE TABLE Cubos of Tipo_Cubo;
+/
+INSERT INTO Cubos VALUES(Tipo_Cubo (10, 10, 10));
+/
+INSERT INTO Cubos VALUES(Tipo_Cubo (3, 4, 5));
+/
+/* Consultamos la tabla de objetos */
+SELECT * FROM Cubos;
+/
+/* Ejecutamos los metodos de la tabla de objetos */
+SELECT c.largo, c.ancho,c.alto,c.volumen(), c.superficie () 
+FROM Cubos c
+WHERE c.largo = 10; 
+
+
